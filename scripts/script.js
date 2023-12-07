19d1359d86077538b26c7bc8aa5d1ba94198cbc7
pmmachadov <pmmachadov@gmail.com>
2023-12-07 19:50:39 +0100

    Code reorganization

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-alert("hello!");
-
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
-})
